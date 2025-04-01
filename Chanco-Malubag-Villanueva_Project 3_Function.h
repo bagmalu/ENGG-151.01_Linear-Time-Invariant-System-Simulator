ac5c37540f90ab37d4b526cfc6671f44d8b9ebb5
Lorraine Malubag <malubaglorraine@gmail.com>
2025-04-01 19:44:34 +0800

    made files lang

---
+++ Chanco-Malubag-Villanueva_Project 3_Function.h
@@ -0,0 +1,18 @@
+// Chanco, Malubag, Villanueva
+// 3 BS Computer Engineering
+
+// ENGG 151.01
+// Project 3: Linear Time-Invariant System Simulator
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+
+#endif // FUNCTION_H
---
+++ Chanco-Malubag-Villanueva_Project 3_main.cpp
@@ -0,0 +1,20 @@
+// Chanco, Malubag, Villanueva
+// 3 BS Computer Engineering
+
+// ENGG 151.01
+// Project 3: Linear Time-Invariant System Simulator
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <cmath>
+
+#include "Chanco-Malubag-Villanueva_Project 3_Function.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+}
