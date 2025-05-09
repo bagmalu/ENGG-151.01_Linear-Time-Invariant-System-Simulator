221db9f21f991e025fbf783df9163364ab49fda4
nyannerds <maria.chanco@student.ateneo.edu>
2025-05-10 02:41:46 +0800

    Update Chanco-Malubag-Villanueva_Project-3_main.cpp

--- Chanco-Malubag-Villanueva_Project-3_main.cpp
+++ Chanco-Malubag-Villanueva_Project-3_main.cpp
@@ -20,105 +20,87 @@ int main(int argc, char *argv[])
   cout << "\nLinear Time-Invariant System Simulator"
        << "\nType \"help\" for more information." << endl;
 
-  // //log file, append mode
-  // ofstream logfile;
-  // logfile.open("ltisim-log.txt", ios::app);
-
-  // if(!logfile) //file does not exist
-  //   logfile.open("ltisim-log.txt");
-    
-  // if(!logfile.is_open()) //file cannot be opened
-  //   cout << "ERROR: Unable to open ltisim-log.txt for appending." << endl;
-
-  //program loop
   string userInput, command, filename;
   double next_input;
 
-  int nNonRecursiveCoefs, nRecursiveCoefs;
-  double * acoef = NULL;
-  double * bcoef = NULL;
+  int nNonRecursiveCoefs = 0, nRecursiveCoefs = 0;
+  double *acoef = NULL;
+  double *bcoef = NULL;
 
-  int n = 0;
-  int nSamples = 0;
-  double * inputs = NULL;
-  double * outputs = NULL;
+  int n = 0, nSamples = 0;
+  double *inputs = NULL;
+  double *outputs = NULL;
 
-  double * input_samples = NULL;
-  double * output_samples = NULL;
+  double *input_samples = NULL;
+  double *output_samples = NULL;
 
-  while(true)
+  while (true)
   {
-    //prompt for user inputs
     cout << "\nltisim> ";
     getline(cin, userInput);
     transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
-    
+
     stringstream ss(userInput);
 
-    //if input is empty
-    if(userInput.empty())
-      cout << "\nERROR: No command has been specified.\n";
-
-    //if input is any floating point number
-    else if(ss >> next_input) 
+    if (userInput.empty())
     {
-      if(ss.eof())
+      cout << "\nERROR: No command has been specified.\n";
+    }
+    else if (ss >> next_input)
+    {
+      if (ss.eof())
       {
-        if(acoef == NULL || bcoef == NULL)
+        if (acoef == NULL || bcoef == NULL)
         {
           cout << "\nERROR: No LTI system has been defined yet.\n";
         }
         else
         {
-          //number is treated as next input to the system
+          // Simulate single input value
+          double temp_input[1] = {next_input};
+          double temp_output[1];
 
-          compute_outputs(acoef, bcoef, inputs, outputs, nNonRecursiveCoefs, nRecursiveCoefs, input_samples, nSamples, output_samples);
+          compute_outputs(acoef, bcoef, inputs, outputs,
+                          nRecursiveCoefs, nNonRecursiveCoefs,
+                          temp_input, 1, temp_output);
 
-          cout << " \t" << output_samples[nSamples-1] << endl;
-          // logfile << inputs[i] << "\t" << outputs[i] << endl;
+          cout << " \t" << temp_output[0] << endl;
         }
       }
       else
+      {
         cout << "\nERROR: Floating point number contains extra characters.\n";
+      }
     }
-
-    //if input is any string
     else
     {
       ss.clear();
       ss.str(userInput);
 
-      if(ss >> command) 
+      if (ss >> command)
       {
-        if(command == "help")
+        if (command == "help")
         {
           cout << "\nCommands:\n"
                << "  help               - displays this message\n"
                << "  system <filename>  - extracts input coefficients from a file \n"
                << "  <number>           - inputs number to the system\n"
                << "  signal <filename>  - extracts an input signal from a file\n"
-               << "  clear              - clears all memory and restarts simulation\n"  
+               << "  clear              - clears all memory and restarts simulation\n"
                << "  exit               - terminates the program\n";
         }
-        else if(command == "system")
+        else if (command == "system")
         {
-          if(!(ss >> filename)) 
-            cout << "\nERROR: No filename has been specified.\n"; 
+          if (!(ss >> filename))
+            cout << "\nERROR: No filename has been specified.\n";
           else
           {
-            if(extractSystem (filename, nNonRecursiveCoefs, nRecursiveCoefs, acoef, bcoef) == NULL)
+            if (extractSystem(filename, nNonRecursiveCoefs, nRecursiveCoefs, acoef, bcoef) == NULL)
             {
-              // logfile << "\nnew system\n" << endl;
-              // for(int i=0; i<nNonRecursiveCoefs; i++)
-              //   logfile << "b(" << i << ") = " << bcoef[i] << endl;
-              // for(int i=0; i<nRecursiveCoefs; i++)
-              //   logfile << "a(" << i+1 << ") = " << acoef[i] << endl;
-              // logfile << "\nready\n";
-
               cout << "\nSystem obtained from \"" << filename << "\"."
                    << " recursive coefs: " << nRecursiveCoefs << ","
                    << " nonrecursive coefs: " << nNonRecursiveCoefs << endl;
-              
+
               delete[] inputs;
               delete[] outputs;
               delete[] input_samples;
@@ -131,37 +113,37 @@ int main(int argc, char *argv[])
             }
           }
         }
-        else if(command == "signal")
+        else if (command == "signal")
         {
-          if(!(ss >> filename)) 
-            cout << "\nERROR: No filename has been specified.\n"; 
+          if (!(ss >> filename))
+            cout << "\nERROR: No filename has been specified.\n";
           else
           {
-            if(acoef == NULL || bcoef == NULL)
+            if (acoef == NULL || bcoef == NULL)
             {
               cout << "\nERROR: No LTI system has been defined yet.\n";
             }
-            else if(extractSignal (filename, n, nSamples, input_samples) != NULL)
+            else if (extractSignal(filename, n, nSamples, input_samples) != NULL)
             {
               cout << "\nSignal obtained from \"" << filename << "\"."
                    << " start index: " << n << ","
                    << " duration: " << nSamples << endl;
 
-              compute_outputs(acoef, bcoef, inputs, outputs, nNonRecursiveCoefs, nRecursiveCoefs, input_samples, nSamples, output_samples);
+              compute_outputs(acoef, bcoef, inputs, outputs,
+                              nRecursiveCoefs, nNonRecursiveCoefs,
+                              input_samples, nSamples, output_samples);
 
-              if(nSamples < 10)
+              if (nSamples < 10)
               {
-                for(int i=0; i<nSamples; i++)
+                for (int i = 0; i < nSamples; i++)
                 {
                   cout << input_samples[i] << "\t" << output_samples[i] << endl;
-                  // logfile << inputs[i] << "\t" << outputs[i] << endl;
                 }
               }
-              //else cout & logfile << summary of the number of inputs simulated << endl;
             }
           }
         }
-        else if(command == "clear")
+        else if (command == "clear")
         {
           delete[] acoef;
           delete[] bcoef;
@@ -176,15 +158,17 @@ int main(int argc, char *argv[])
           n = 0;
           nSamples = 0;
 
-          // logfile << "\ncleared\n";
           cout << "\nAll memory has been cleared.\n";
         }
-        else if(command == "exit")
+        else if (command == "exit")
         {
           cout << "\nProgram has been terminated.\n";
-          return(0);
+          return 0;
         }
-        else cout << "\nERROR: Command \"" << command << "\" does not exist.\n";
+        else
+        {
+          cout << "\nERROR: Command \"" << command << "\" does not exist.\n";
+        }
       }
     }
   }
